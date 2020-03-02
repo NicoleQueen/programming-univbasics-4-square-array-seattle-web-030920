977d34f8cc648efd6450c145515caccb17af3e28
QIuyan Peng <nicolepeng99@gmail.com>
2020-03-02 06:21:37 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   counter = 0
   new_array = []
   while counter < array.length do
-    new_array.sort(array[counter] * array[counter])
+    new_array.push(array[counter] * array[counter])
     counter++
   end
     return new_array
