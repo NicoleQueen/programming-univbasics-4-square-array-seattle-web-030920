680cd7228acdeadbb48d273027e25f6303a14a01
QIuyan Peng <nicolepeng99@gmail.com>
2020-03-02 06:12:49 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   counter = 0
   new_array = []
   while counter < array.length do
-    new_array.push(array[counter])
+    new_array.push(square_array[counter])
     counter++
     return new_array
 end
